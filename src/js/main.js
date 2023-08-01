5e12b5c05b41a3c45b9394e791aca179df341910
Mariia-Kovalenko <mariya.kovalenko.2000@gmail.com>
2023-08-01 21:54:35 +0300

    added responsive header burger menu

--- src/js/main.js
+++ src/js/main.js
@@ -3,12 +3,22 @@
 import sum from './module/sum.js';
 
 const burger = document.querySelector('.burger');
+const nav = document.querySelector('.nav');
 
 burger.addEventListener('click', () => {
     if (burger.classList.contains('close')) {
         burger.classList.remove('close');
+        if (nav.classList.contains('open')) {
+            nav.classList.remove('open')
+        }
     } else {
         burger.classList.add('close');
+        if (!nav.classList.contains('open')) {
+            nav.classList.add('open')
+        }
     }
     
 })
+
+
+
