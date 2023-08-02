67df1720c1b3beeac8b53958725a671010b738b6
Mariia-Kovalenko <mariya.kovalenko.2000@gmail.com>
2023-08-02 16:04:25 +0300

    finished first section

--- dist/js/main.js
+++ dist/js/main.js
@@ -16,64 +16,16 @@
   \************************/
 /***/ (function(__unused_webpack___webpack_module__, __webpack_exports__, __webpack_require__) {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _module_sum_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./module/sum.js */ \"./src/js/module/sum.js\");\n\r\n\r\n\r\n\r\nconst burger = document.querySelector('.burger');\r\nconst nav = document.querySelector('.nav');\r\n\r\nburger.addEventListener('click', () => {\r\n    if (burger.classList.contains('close')) {\r\n        burger.classList.remove('close');\r\n        if (nav.classList.contains('open')) {\r\n            nav.classList.remove('open')\r\n        }\r\n    } else {\r\n        burger.classList.add('close');\r\n        if (!nav.classList.contains('open')) {\r\n            nav.classList.add('open')\r\n        }\r\n    }\r\n    \r\n})\r\n\r\n\r\n\r\n\n\n//# sourceURL=webpack://gulp-starter/./src/js/main.js?");
-
-/***/ }),
-
-/***/ "./src/js/module/sum.js":
-/*!******************************!*\
-  !*** ./src/js/module/sum.js ***!
-  \******************************/
-/***/ (function(__unused_webpack___webpack_module__, __webpack_exports__, __webpack_require__) {
-
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": function() { return /* binding */ sum; }\n/* harmony export */ });\n\r\nfunction sum(a,b) {\r\n    return a + b;\r\n} \n\n//# sourceURL=webpack://gulp-starter/./src/js/module/sum.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n\r\n\r\nconst burger = document.querySelector('.burger');\r\nconst nav = document.querySelector('.nav');\r\n\r\nburger.addEventListener('click', () => {\r\n    if (burger.classList.contains('close')) {\r\n        burger.classList.remove('close');\r\n        if (nav.classList.contains('open')) {\r\n            nav.classList.remove('open')\r\n        }\r\n    } else {\r\n        burger.classList.add('close');\r\n        if (!nav.classList.contains('open')) {\r\n            nav.classList.add('open')\r\n        }\r\n    }\r\n    \r\n});\r\n\r\n// slider\r\nconst slider = document.querySelector('.slider__slides');\r\nconst slides = document.querySelectorAll('.slide');\r\n\r\nconst prevBtn = document.querySelector('#prevBtn');\r\nconst nextBtn = document.querySelector('#nextBtn');\r\n\r\nlet counter = 0;\r\n// const size = slider.clientWidth;\r\n\r\nnextBtn.addEventListener('click', () => {\r\n    if (counter >= slides.length - 1) {\r\n        return;\r\n    }\r\n    slider.style.transition = 'transform 0.4s ease-in-out';\r\n    counter++;\r\n    \r\n    if (counter === slides.length - 1) {\r\n        prevBtn.classList.remove('last')\r\n        nextBtn.classList.add('last')\r\n    }\r\n    slider.style.transform = 'translateX(' + (-document.querySelector('.slider__slides').clientWidth * counter) + 'px)';\r\n});\r\n\r\nprevBtn.addEventListener('click', () => {\r\n    if (counter <= 0) {\r\n        return;\r\n    }\r\n    slider.style.transition = 'transform 0.4s ease-in-out';\r\n    counter--;\r\n\r\n    if (counter === 0) {\r\n        nextBtn.classList.remove('last')\r\n        prevBtn.classList.add('last')\r\n    }\r\n\r\n    slider.style.transform = 'translateX(' + (-document.querySelector('.slider__slides').clientWidth * counter) + 'px)';\r\n});\r\n\r\nfunction reportWindowSize() {\r\n    const width = window.innerWidth;\r\n    console.log(width);\r\n\r\n    if (width > 1024) {\r\n        [].forEach.call(slides, function(slide) {\r\n            // do whatever\r\n            slide.style.minWidth = '700px';\r\n        });\r\n    }\r\n\r\n    if (width <= 1024) {\r\n        [].forEach.call(slides, function(slide) {\r\n            // do whatever\r\n            slide.style.minWidth = '550px';\r\n        });\r\n    }\r\n\r\n    if (width <= 870) {\r\n        [].forEach.call(slides, function(slide) {\r\n            // do whatever\r\n            slide.style.minWidth = '100%';\r\n        });\r\n    }\r\n}\r\n\r\n// window.addEventListener(\"resize\", reportWindowSize);\r\n\n\n//# sourceURL=webpack://gulp-starter/./src/js/main.js?");
 
 /***/ })
 
 /******/ 	});
 /************************************************************************/
-/******/ 	// The module cache
-/******/ 	var __webpack_module_cache__ = {};
-/******/ 	
-/******/ 	// The require function
-/******/ 	function __webpack_require__(moduleId) {
-/******/ 		// Check if module is in cache
-/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
-/******/ 		if (cachedModule !== undefined) {
-/******/ 			return cachedModule.exports;
-/******/ 		}
-/******/ 		// Create a new module (and put it into the cache)
-/******/ 		var module = __webpack_module_cache__[moduleId] = {
-/******/ 			// no module.id needed
-/******/ 			// no module.loaded needed
-/******/ 			exports: {}
-/******/ 		};
-/******/ 	
-/******/ 		// Execute the module function
-/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
-/******/ 	
-/******/ 		// Return the exports of the module
-/******/ 		return module.exports;
-/******/ 	}
+/******/ 	// The require scope
+/******/ 	var __webpack_require__ = {};
 /******/ 	
 /************************************************************************/
-/******/ 	/* webpack/runtime/define property getters */
-/******/ 	!function() {
-/******/ 		// define getter functions for harmony exports
-/******/ 		__webpack_require__.d = function(exports, definition) {
-/******/ 			for(var key in definition) {
-/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
-/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
-/******/ 				}
-/******/ 			}
-/******/ 		};
-/******/ 	}();
-/******/ 	
-/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
-/******/ 	!function() {
-/******/ 		__webpack_require__.o = function(obj, prop) { return Object.prototype.hasOwnProperty.call(obj, prop); }
-/******/ 	}();
-/******/ 	
 /******/ 	/* webpack/runtime/make namespace object */
 /******/ 	!function() {
 /******/ 		// define __esModule on exports
@@ -90,7 +42,8 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = __webpack_require__("./src/js/main.js");
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/js/main.js"](0, __webpack_exports__, __webpack_require__);
 /******/ 	
 /******/ })()
 ;
--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,5 @@
 "use strict";
 
-import sum from './module/sum.js';
-
 const burger = document.querySelector('.burger');
 const nav = document.querySelector('.nav');
 
@@ -18,7 +16,71 @@ burger.addEventListener('click', () => {
         }
     }
     
-})
+});
 
+// slider
+const slider = document.querySelector('.slider__slides');
+const slides = document.querySelectorAll('.slide');
 
+const prevBtn = document.querySelector('#prevBtn');
+const nextBtn = document.querySelector('#nextBtn');
 
+let counter = 0;
+// const size = slider.clientWidth;
+
+nextBtn.addEventListener('click', () => {
+    if (counter >= slides.length - 1) {
+        return;
+    }
+    slider.style.transition = 'transform 0.4s ease-in-out';
+    counter++;
+    
+    if (counter === slides.length - 1) {
+        prevBtn.classList.remove('last')
+        nextBtn.classList.add('last')
+    }
+    slider.style.transform = 'translateX(' + (-document.querySelector('.slider__slides').clientWidth * counter) + 'px)';
+});
+
+prevBtn.addEventListener('click', () => {
+    if (counter <= 0) {
+        return;
+    }
+    slider.style.transition = 'transform 0.4s ease-in-out';
+    counter--;
+
+    if (counter === 0) {
+        nextBtn.classList.remove('last')
+        prevBtn.classList.add('last')
+    }
+
+    slider.style.transform = 'translateX(' + (-document.querySelector('.slider__slides').clientWidth * counter) + 'px)';
+});
+
+function reportWindowSize() {
+    const width = window.innerWidth;
+    console.log(width);
+
+    if (width > 1024) {
+        [].forEach.call(slides, function(slide) {
+            // do whatever
+            slide.style.minWidth = '700px';
+        });
+    }
+
+    if (width <= 1024) {
+        [].forEach.call(slides, function(slide) {
+            // do whatever
+            slide.style.minWidth = '550px';
+        });
+    }
+
+    if (width <= 870) {
+        [].forEach.call(slides, function(slide) {
+            // do whatever
+            slide.style.minWidth = '100%';
+        });
+    }
+}
+
+// window.addEventListener("resize", reportWindowSize);
